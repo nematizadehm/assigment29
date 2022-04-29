346ee7d2cd3731501fc4e72c5faea1b304bbfc61
Mona nematizadeh <95125153+nematizadehm@users.noreply.github.com>
2022-04-29 17:07:51 +0430

    Update index.php

--- task project/index.php
+++ task project/index.php
@@ -3,35 +3,58 @@
 include "header.php";
 include "database.php";
 
-$notes = $db->query("SELECT * FROM taskdata")
+$tasks = $db->query("SELECT * FROM taskdata")
 ?>
 
 <div class="container">
-                    <form method="post" action="add_note_process.php">         
-                        <label for="exampleInputPassword1" class="form-label"> تسک خود را وارد کنید</label>
+                    <form method="post" action="add_task.php">         
+                    <label for="exampleInputPassword1" class="form-label"> عنوان خود را وارد کنید</label>
                         <div class="form-floating">
                         <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
                         <label for="floatingTextarea">Comments</label>
                         </div>
-                        
-                        <label for="exampleInputPassword1" class="form-label"> رنگ را انتخاب کنید</label>
+
+                       <label for="exampleInputPassword1" class="form-label"> تسک خود را وارد کنید</label>
+                        <div class="form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                        <label for="floatingTextarea">Comments</label>
+                        </div>
+               
+                        <label for="exampleInputPassword1" class="form-label"> رنگ </label>
                         <div class="form-floating">
                             <input type="color" name="color" value="#063d7c" >
                         </div>
-                        <label for="exampleInputPassword1" class="form-label"> تاریخ را انتخاب کنید</label>
+                        <label for="exampleInputPassword1" class="form-label"> تاریخ   </label>
                         <div class="col-3">
                             <input type="date" value="" name="date" >
                         </div>
-                        <label for="exampleInputPassword1" class="form-label"> زمان را انتخاب کنید</label>
+                        <label for="exampleInputPassword1" class="form-label"> زمان   </label>
                         <div class="col-3">
                         <input type="time" value="" name="time" >
                         </div>
                         <br>
                         <button type="submit" class="btn btn-primary">ذخیره</button>
+                
                     </form>
  </div>
 
-    
+ <div class="row mt-5" >
+            <?php foreach ($tasks as $task): ?>
+                <div class=" col-lg-3 col-md-6 col-sm-12 col-12 position-relative">
+                    <div class="card text-white mb-3 text-start" style="background:<?php echo $task["color"] ?>; ">
+                        <div class="card-header stroke "> <h4 class="d-block"><?php echo $task["title"] ?> </h4>
+                        </div>
+                        <div class="card-body ">
+                            <h5 class="card-text mb-3 stroke " id="text_note"><?php echo $task["text"] ?></h5>
+                            <p class="card-text mb-2 stroke fs-6"> <i class="far fa-alarm-clock"></i> <?php echo $task["time"] ?> </p>
+                            <p class="card-text  stroke fs-6"><i class="far fa-calendar-alt"></i>  <?php echo $task["date"] ?> </p>
+                        </div>
+                       
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+        </div>
     
        
 <?php
